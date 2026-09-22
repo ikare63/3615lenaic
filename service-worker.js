505314308fdfdcea19dd6e86a2e916b3dbfb61f8
ikare63 <lenaicandre59@gmail.com>
2026-09-22 19:45:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='3615-quotidien-v915';
+const CACHE_VERSION='3615-rappels-v918';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('notificationclick',event=>{
